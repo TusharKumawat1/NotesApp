656d0f1dd1c207776584679a8ca2c0fa94c11222
tushar <tusharkumawat9694@gmail.com>
2024-04-15 15:03:23 +0530

    working

--- my-app/src/app/api/auth/signup/route.ts
+++ my-app/src/app/api/auth/signup/route.ts
@@ -1,13 +1,12 @@
-import type { NextRequest,NextResponse} from 'next/server'
+import { NextRequest,NextResponse} from 'next/server'
 import bcrypt from "bcrypt"
 import { dbConnection } from '@/utils/db'
 import jwt from "jsonwebtoken"
 dbConnection();
-export const POST = async (
-    req: NextRequest,
-    res: NextResponse)=> {
+export const POST = async ( req: NextRequest)=> {
         try {
-            console.log(req.body)
+            console.log(await req.json())
+            return NextResponse.json({success:true})
         } catch (error) {
             console.log("Failed to signUp : ", error)
         }
--- my-app/src/utils/db.ts
+++ my-app/src/utils/db.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 export const dbConnection=async()=>{
     try {
-        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI!)
+        console.log(process.env.MONGO_URI!)
+        await mongoose.connect(process.env.MONGO_URI!)
     } catch (error) {
         console.log(`Faild to connect server : ${error}`)
     }
