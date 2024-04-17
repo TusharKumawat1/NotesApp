bc2061e57ffca54020387c3565deacea0fb01dda
tushar <tusharkumawat9694@gmail.com>
2024-04-17 16:12:05 +0530

    working

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtDecode } from "jwt-decode";
-import jose from "jose"
 export const middleware =async (req: NextRequest) => {
     try {
         const token = req.headers.get("token")
         if (!token) return NextResponse.json({ success: false, error: "Not Authorized" }, { status: 409 })
         const decodedToken =jwtDecode(token);
-        // const { plaintext, protectedHeader } = await jose.compactDecrypt(token,process.env.JWT_SEC!)
         console.log(decodedToken)
         return NextResponse.next()
     } catch (error) {
