357cd518a130386156e037147423528b36c3cf15
tushar <tusharkumawat9694@gmail.com>
2024-04-20 12:44:11 +0530

    qf

--- my-app/src/utils/db.ts
+++ my-app/src/utils/db.ts
@@ -3,7 +3,7 @@ export const dbConnection=async()=>{
     try {
         console.log(process.env.MONGO_URI!)
         await mongoose.connect(process.env.MONGO_URI!,{
-            serverSelectionTimeoutMS: 10000, 
+            serverSelectionTimeoutMS: 30000, 
           })
     } catch (error) {
         console.log(`Faild to connect server : ${error}`)
